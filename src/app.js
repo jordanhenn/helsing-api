c69e3a2f276f6a6683bb16f22907d7235a2f4650
Jordan Henn <jordanwalkerhenn@gmail.com>
2021-02-10 22:57:06 -0800

    router/service files

--- src/app.js
+++ src/app.js
@@ -1,28 +1,31 @@
-require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const ReserveStudyRouter = require('./reservestudy/reservestudy-router')
+const SustainmentRouter = require('./sustainment/sustainment-router')
+const TimeAndMaterialRouter = require('./timeandmaterial/timeandmaterial-router')
+const EmployeeRouter = require('./employee/employee-router')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
-
-app.use(morgan(morganOption))
-app.use(helmet())
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test',
+}))
 app.use(cors())
+app.use(helmet())
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!')
-})
+app.use(express.json());
+app.use('/api/reservestudy', ReserveStudyRouter)
+app.use('/api/sustainment', SustainmentRouter)
+app.use('/api/timeandmaterial', TimeAndMaterialRouter)
+app.use('/api/employee', EmployeeRouter)
 
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
-        response = { errpr: { message: 'server error' }}
+        response = { error: { message: 'server error' }}
     } else {
         console.error(error)
         response = { message: error.message, error }
