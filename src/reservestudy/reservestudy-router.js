763202362c488e13a422a18e06e8212879ace0f0
Jordan Henn <jordanwalkerhenn@gmail.com>
2021-02-15 22:28:39 -0800

    reserve study and sustainment routers

---
+++ src/reservestudy/reservestudy-router.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const ReserveStudyService = require('./reservestudy-service')
+const jsonBodyParser = express.json()
+
+const ReserveStudyRouter = express.Router()
+
+ReserveStudyRouter
+  .route('/')
+  .get((req, res, next) => {
+    ReserveStudyService.getAllItems(req.app.get('db'))
+      .then(items => {
+        res.json(items)
+      })
+      .catch(next)
+})
+  .post(jsonBodyParser, (req, res, next) => {
+    const { association, manager_firstname, manager_email, assigned_to, fy_end, client_number, total_price, csa, scope, retainer, ccrs, hoa_questionnaire, budget, site_plan, reserve_study, annual_review, income_statement, balance_sheet, additional_notes, date_added } = req.body
+    const newStudy = { association, manager_firstname, manager_email, assigned_to, fy_end, client_number, total_price, csa, scope, retainer, ccrs, hoa_questionnaire, budget, site_plan, reserve_study, annual_review, income_statement, balance_sheet, additional_notes, date_added }
+
+      if (association == null)
+        return res.status(400).json({
+          error: `Missing association name in request body`
+    })
+    
+    ReserveStudyService.postReserveStudy(
+      req.app.get('db'),
+      newStudy
+    )
+      .then(study => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${study.id}`))
+          .json(study)
+      })
+      .catch(next)
+  })
+
+ReserveStudyRouter
+  .route('/:rs_id')
+  .all(checkReserveStudyExists)
+  .get((req, res) => {
+    res.json(res.study)
+  })
+  .delete((req, res, next) => {
+      ReserveStudyService.deleteReserveStudy(
+          req.app.get('db'),
+          res.study.id
+      )
+      .then(
+          res.status(204)
+      )
+      .catch(next)
+  })
+  .put((req, res, next) => {
+    const { association, manager_firstname, manager_email, assigned_to, fy_end, client_number, total_price, csa, scope, retainer, ccrs, hoa_questionnaire, budget, site_plan, reserve_study, annual_review, income_statement, balance_sheet, draft_billed, final_billed, date_in_queue, additional_notes } = req.body
+    const updatedInfo = { association, manager_firstname, manager_email, assigned_to, fy_end, client_number, total_price, csa, scope, retainer, ccrs, hoa_questionnaire, budget, site_plan, reserve_study, annual_review, income_statement, balance_sheet, draft_billed, final_billed, date_in_queue, additional_notes }
+    ReserveStudyService.updateReserveStudy(
+        req.app.get('db'),
+        res.study.id,
+        updatedInfo
+      )
+      .then(study => {
+        res
+          .status(204)
+          .location(path.posix.join(req.originalUrl, `/${study.id}`))
+          .json(study)
+      })
+      .catch(next)
+
+})
+
+async function checkReserveStudyExists(req, res, next) {
+  try {
+    const study = await ReserveStudyService.getReserveStudyById(
+      req.app.get('db'),
+      req.params.rs_id
+    )
+
+    if (!study)
+      return res.status(404).json({
+        error: `Reserve study doesn't exist`
+    })
+
+    res.study = study
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
+
+
+module.exports = ReserveStudyRouter
---
+++ src/sustainment/sustainment-router.js
@@ -0,0 +1,94 @@
+const express = require('express')
+const SustainmentService = require('./Sustainment-service')
+const jsonBodyParser = express.json()
+
+const SustainmentRouter = express.Router()
+
+SustainmentRouter
+  .route('/')
+  .get((req, res, next) => {
+    SustainmentService.getAllItems(req.app.get('db'))
+      .then(items => {
+        res.json(items)
+      })
+      .catch(next)
+})
+  .post(jsonBodyParser, (req, res, next) => {
+    const { association, manager_firstname, manager_email, fy_end, client_number, assigned_to, total_price, contract, retainer, additional_notes } = req.body
+    const newStudy = { association, manager_firstname, manager_email, fy_end, client_number, assigned_to, total_price, contract, retainer, additional_notes }
+    newStudy.date_added = new Date()
+
+      if (association == null)
+        return res.status(400).json({
+          error: `Missing association name in request body`
+    })
+    
+    SustainmentService.postSustainment(
+      req.app.get('db'),
+      newStudy
+    )
+      .then(study => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${study.id}`))
+          .json(study)
+      })
+      .catch(next)
+  })
+
+SustainmentRouter
+  .route('/:s_id')
+  .all(checkSustainmentExists)
+  .get((req, res) => {
+    res.json(res.study)
+  })
+  .delete((req, res, next) => {
+      SustainmentService.deleteSustainment(
+          req.app.get('db'),
+          res.study.id
+      )
+      .then(
+          res.status(204)
+      )
+      .catch(next)
+  })
+  .put((req, res, next) => {
+    const { association, manager_firstname, manager_email, fy_end, client_number, assigned_to, total_price, contract, retainer, worksheets_yr1, worksheets_yr2, worksheets_yr3, yr1_billed, yr2_billed, yr3_billed, sustainment_letter, additional_notes } = req.body
+    const updatedInfo = { association, manager_firstname, manager_email, fy_end, client_number, assigned_to, total_price, contract, retainer, worksheets_yr1, worksheets_yr2, worksheets_yr3, yr1_billed, yr2_billed, yr3_billed, sustainment_letter, additional_notes }
+    SustainmentService.updateSustainment(
+        req.app.get('db'),
+        res.study.id,
+        updatedInfo
+      )
+      .then(study => {
+        res
+          .status(204)
+          .location(path.posix.join(req.originalUrl, `/${study.id}`))
+          .json(study)
+      })
+      .catch(next)
+
+})
+
+async function checkSustainmentExists(req, res, next) {
+  try {
+    const study = await SustainmentService.getSustainmentById(
+      req.app.get('db'),
+      req.params.s_id
+    )
+
+    if (!study)
+      return res.status(404).json({
+        error: `Reserve study doesn't exist`
+    })
+
+    res.study = study
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
+
+
+module.exports = SustainmentRouter
