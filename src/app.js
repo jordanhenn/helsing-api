988ad1e010df6597eae0954782ecb30973a557fe
Jordan Henn <jordanwalkerhenn@gmail.com>
2021-03-13 18:14:56 -0800

    cors bug

--- src/app.js
+++ src/app.js
@@ -13,7 +13,11 @@ const app = express()
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
   skip: () => NODE_ENV === 'test',
 }))
-app.use(cors({origin: true}))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(helmet())
 
 app.use(express.json());
